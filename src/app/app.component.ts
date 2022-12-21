605160f8245bae91c9d818119511886cc0a63102
Nesma Taha <nesmataha91@gmail.com>
2022-12-21 02:09:48 +0200

    typing challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+
+  onChangeValue(event: Event) {
+    this.enteredText = (event.target as HTMLInputElement).value;
+  }
+
+  copmare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return enteredLetter === randomLetter ? 'correct' : 'incorrect';
+  }
 }
